4ffd1eb47ab8d0c3a3dbc2277705f0e65f88cbd0
Franziska Neumann-Lehmann <rubenhardylehmann@gmail.com>
2025-09-04 22:54:27 +0200

    aufgabe 1 finished

--- Aufgabe1/drehfreudig.py
+++ Aufgabe1/drehfreudig.py
@@ -109,38 +109,56 @@ def drawTree(tree: dict, weights):
     def dict_depth(d):
         return 1 + (max(map(dict_depth, d.values()))
                         if not isinstance(d, int) else 0)
-
-    y = 300 * (dict_depth(tree) +1) + 300
+    yscale = 200
+    y = yscale * (dict_depth(tree) +1) + yscale
     img = Image.new("RGB", (1000, y))
     draw = ImageDraw.Draw(img)
     draw.rectangle((0, 0, 1000, y), fill="white")
 
-    def drawRectangles(node, x):
+    def drawStructure(node, x):
         for i in node:
             if i == "0.0":
-                draw.rectangle((0, 0, 1000, 300), fill="orange", outline="black", width=5)
+                draw.rectangle((0, 0, 1000, yscale), fill="orange", outline="black", width=5)
             if i != "weight":
                 # print(node[i]["weight"], i[-1])
                 # print(int(i[-1]) * (1000/node[i]["weight"]))
                 print(x + (1000/(node[i]["weight"] * len(node[i])))*int(i[-1]))
                 print("key:",i)
                 # x = int(i[-1]) * node[i]["weight"]
+
             try:
                 for j in node[i]:
                     if j == "weight":
                         continue
                     # print(x + (1000/node[i]["weight"]))
                     width = (1000/node[i][j]["weight"]) * int(j[-1]) + (1000/node[i]["weight"]) * int(i[-1])
-                    draw.rectangle((x + width, (int(j[0])) * 300,
-                                    x + width + (1000/node[i][j]["weight"]), y/2), # (int(j[0]) + 1)*300),
+
+                    rx1 = x + width
+                    ry1 = (int(j[0])) * yscale
+                    rx2 = x + width + (1000/node[i][j]["weight"])
+                    ry2 = y/2
+
+                    lp1 = x + (1000/node[i]["weight"] * (int(i[-1])+.5)), int(i[0])*yscale + yscale*.3,
+                    lp2 = x + width + (1000/node[i][j]["weight"])/2, ry1 + yscale*.3
+                    lp = (*lp1, *lp2)
+
+                    draw.rectangle((rx1, ry1, rx2, ry2), # (int(j[0]) + 1)*300),
                                     fill="orange", outline="black", width=5)
-                    #x1 += node[i]["weight"]
-                    #afterweight = node[i]["weight"] * len(node[i])
-                drawRectangles(node[i], x + int(i[-1]) * (1000/node[i]["weight"]))
-            except TypeError as e:
+
+                    draw.line(lp, fill="blue", width=5)
+
+                    draw.circle(lp1, radius=8, fill="red")
+                    draw.circle(lp2, radius=8, fill="red")
+                    # x1 += node[i]["weight"]
+                    # afterweight = node[i]["weight"] * len(node[i])
+                drawStructure(node[i], x + int(i[-1]) * (1000/node[i]["weight"]))
+            except TypeError:
                 pass
 
-    drawRectangles(tree, 0)
+    drawStructure(tree, 0)
+
+    counterPart = img.crop((0, 0, 1000, int(y/2))).rotate(180)
+    img.paste(counterPart, (0, int(y/2)))
     img.show()
 
 
