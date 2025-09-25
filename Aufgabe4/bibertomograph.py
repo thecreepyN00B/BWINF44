07e9eb870091dcbe1ae4ac16df09bcc85f9d7b97
Quokkk <roedelkonrad@gmail.com>
2025-09-25 19:40:47 +0200

    rub

--- Aufgabe4/bibertomograph.py
+++ Aufgabe4/bibertomograph.py
@@ -9,31 +9,49 @@ class Field:
 
 class Board:
     fields = []
+    n = 0
     numbers = []
+    spalten = []
+    zeilen = []
+    diagonalen_ol_ur = []
+    diagonalen_ul_or = []
 
-    def __init__(self, n, list=[0, 0, 0, 0, 0, 0]):
-        if len(list) % 2 == 0:
-            pass
-        else:
-            raise Exception("nix das Quadrat werden :(")
+    def __init__(self, n, spalten, zeilen, diagonalen_ol_ur, diagonalen_ul_or):
+        for i in range(n):
+            self.fields.append([Field() for j in range(n)])
 
-        x = int(len(list) / 2)
-        for i in range(x):
-            self.fields.append([Field() for i in range(x)])
-        self.numbers = list
+        self.numbers = [spalten, zeilen, diagonalen_ol_ur, diagonalen_ul_or]
+        self.n = n
+        self.spalten = spalten
+        self.zeilen = zeilen
+        self.diagonalen_ol_ur = diagonalen_ol_ur
+        self.diagonalen_ul_or = diagonalen_ul_or
 
     def __str__(self):
         list = []
-        for i in range(len(self.fields)):
-            for j in range(len(self.fields)):
+        for i in range(n):
+            for j in range(n):
                 list.append(str(self.fields[i][j]))
         return str(list) + str(self.numbers)
+
+    def printboard(self):
+        list = self.fields.copy()
+        for i in range(self.n):
+            for j in range(self.n):
+                list[i][j] = str(list[i][j])
+            print(list[i])
+        print()
+        print(self.spalten)
+        print(self.zeilen)
+        print(self.diagonalen_ol_ur)
+        print(self.diagonalen_ul_or)
+
 
     def getFieldColor(self, x, y):
         return self.fields[y][x].color
 
     def setFieldColor(self, x, y, col):
-        self.fields[y][x].color = col
+        self.fields[y][x].color = int(col)
 
     def getField(self, x, y):
         return self.fields[y][x]
@@ -69,8 +87,27 @@ class Board:
                 else:
                     pass
             return count
-        else:
-            pass
+        elif zs == 'd_ul':
+            list = []
+            for i in range(-abs(10 - x) + 10)
+            for i in range(len(list)):
+                list[i] = list[i].color
+            for i in list:
+                if i == col:
+                    count += 1
+                else:
+                    pass
+            return count
 
-
-brettttt = Board(zahlen)
+e = open("tomograph07.txt", 'r')
+n = int(e.readline())
+spalten = e.readline().split()
+zeilen = e.readline().split()
+diagonalen_ol_ur = e.readline().split()
+diagonalen_ul_or = e.readline().split()
+e.close
+brettttt = Board(n, spalten, zeilen, diagonalen_ol_ur, diagonalen_ul_or)
+brettttt.printboard()
+#t = open("ausgabe.csv", 'w')
+#t.write(str(brettttt))
+#t.close()
