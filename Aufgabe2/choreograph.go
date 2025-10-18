43bb8b34a1e4a9ee72e6c4799389e8bf54e531d1
scuncgg <sucukmeister21@gmail.com>
2025-10-18 21:38:28 +0200

    aufgabe 2 einlesen von dateien

--- Aufgabe2/choreograph.go
+++ Aufgabe2/choreograph.go
@@ -1,25 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(useFigure("DABCHEFGLIJKPMNO", "PONLMABCHEFDKJIG"))
+	r, err := os.Open("choreo01.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer r.Close()
+
+	scanner := bufio.NewScanner(r)
+
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	// tacts, _ := strconv.Atoi(scanner.Text())
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+
+	figures := make([]Figure, n)
+
+	for i := range n {
+		scanner.Scan()
+		name := scanner.Text()
+
+		scanner.Scan()
+		tacts, _ := strconv.Atoi(scanner.Text())
+
+		scanner.Scan()
+		endpos := scanner.Text()
+
+		figures[i] = MakeFigure(name, tacts, endpos)
+	}
+
+	fmt.Println(figures)
 }
 
-// func readFigures --> Datei ablesen, einzelne Figuren zurueckgeben
+type Figure struct {
+	name   string
+	tacts  int
+	endpos string
+}
+
+func MakeFigure(name string, tacts int, endpos string) Figure {
+	return Figure{name, tacts, endpos}
+}
+
+// func readFigures() // --> Datei ablesen, einzelne Figuren zurueckgeben
 
 func useFigure(config string, input string) string {
-	var prime string = "ABCDEFGHIJKLMNOP"
-	var end string = ""
+	prime := "ABCDEFGHIJKLMNOP"
+	end := ""
+
 	for i := range len(config) {
 		for j := range len(prime) {
 			if config[i] == prime[j] {
-				end += string(input[j]) // Gleiche wie end[i] = input[j]
+				end += string(input[j]) // Ist das Gleiche wie end[i] = input[j]
 				break
 			}
 		}
 	}
+
 	return end
 }
