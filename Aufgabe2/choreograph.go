d5162564591f36b44c973463ba2b0db4c6d8b436
scuncgg <sucukmeister21@gmail.com>
2025-10-25 22:23:22 +0200

    aufgabe 2 bulk fertig, nur noch kriterien uebrig

--- Aufgabe2/choreograph.go
+++ Aufgabe2/choreograph.go
@@ -11,7 +11,21 @@ func main() {
 	figures, tacts := readFigures("choreo01.txt")
 	fmt.Println(figures)
 	fmt.Println(tacts)
+	tacts1 = tacts
+
+	combs := getCombinations(figures, []*Figure{}, startlineup)
+	for _, a := range combs {
+		for _, p := range a {
+			fmt.Println(*p)
+		}
+		fmt.Print("\n")
+	}
+	fmt.Println(tacts1)
 }
+
+const startlineup string = "ABCDEFGHIJKLMNOP"
+
+var tacts1 int
 
 type Figure struct {
 	name   string
@@ -19,11 +33,15 @@ type Figure struct {
 	endpos string
 }
 
-func MakeFigure(name string, tacts int, endpos string) Figure {
-	return Figure{name, tacts, endpos}
+func NewFigure(name string, tacts int, endpos string) *Figure {
+	var fig *Figure = new(Figure)
+	fig.name = name
+	fig.tacts = tacts
+	fig.endpos = endpos
+	return fig
 }
 
-func readFigures(file string) ([]Figure, int) {
+func readFigures(file string) ([]*Figure, int) {
 	r, err := os.Open(file)
 
 	if err != nil {
@@ -42,7 +60,7 @@ func readFigures(file string) ([]Figure, int) {
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
 
-	figures := make([]Figure, n)
+	figures := make([]*Figure, n)
 
 	for i := range n {
 		scanner.Scan()
@@ -54,19 +72,18 @@ func readFigures(file string) ([]Figure, int) {
 		scanner.Scan()
 		endpos := scanner.Text()
 
-		figures[i] = MakeFigure(name, tacts, endpos)
+		figures[i] = NewFigure(name, tacts, endpos)
 	}
 
 	return figures, tacts
 }
 
 func useFigure(config string, input string) string {
-	prime := "ABCDEFGHIJKLMNOP"
 	end := ""
 
 	for i := range len(config) {
-		for j := range len(prime) {
-			if config[i] == prime[j] {
+		for j := range len(startlineup) {
+			if config[i] == startlineup[j] {
 				end += string(input[j]) // Ist das Gleiche wie end[i] = input[j]
 				break
 			}
@@ -74,4 +91,33 @@ func useFigure(config string, input string) string {
 	}
 
 	return end
+}
+
+func getCombinations(list []*Figure, usedFigures []*Figure, lineup string) [][]*Figure {
+	var solutions [][]*Figure
+	var usedtacts int
+
+	for _, p := range usedFigures {
+		usedtacts += p.tacts
+	}
+
+	if usedtacts == tacts1 {
+		if lineup == startlineup {
+			return [][]*Figure{usedFigures}
+		} else {
+			return solutions
+		}
+	}
+	if usedtacts > tacts1 {
+		return solutions
+	}
+
+	for _, p := range list {
+		newlineup := useFigure(p.endpos, lineup)
+		newFigures := make([]*Figure, len(usedFigures))
+		copy(newFigures, usedFigures)
+		solutions = append(solutions, getCombinations(list, append(newFigures, p), newlineup)...)
+	}
+
+	return solutions
 }
